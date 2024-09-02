415c9d9b91f0c03d8b82f0023217eeec242727e1
Dominykas00 <dominykas.svilpa@confindo.com>
2024-09-02 12:31:46 +0300

    maximum loan sum created

--- script.js
+++ script.js
@@ -36,3 +36,47 @@ function closeResult() {
   document.getElementById("result_pop_up").style.display = "none";
   document.getElementById("opacity_container").style.display = "none";
 }
+
+function calculateMaxSum() {
+  let euribor = parseFloat(document.getElementById("euribor").value);
+  let interestRate = parseFloat(document.getElementById("interestRate").value);
+  let loanTerm = parseFloat(document.getElementById("loan_term").value);
+  let householdIncome = parseFloat(document.getElementById("household_income").value);
+  let householdObligations = parseFloat(document.getElementById("household_obligations").value);
+  let houseHoldLivelihoodCosts = parseFloat(document.getElementById("min_livelihood_income").value);
+  let maxDTI = parseFloat(document.getElementById("max_dti").value);
+  event.preventDefault();
+
+  
+  let balanceAfterExpenses = householdIncome - householdObligations - houseHoldLivelihoodCosts;
+  let numberOfPayments = loanTerm * 12;
+  let monthlyInterestRate = (euribor + interestRate) / 100 / 12; // Convert to decimal and then divide by 12 for monthly rate
+
+  const maxMonthlyPayment = calculateMaxMonthlyPayment(householdIncome, householdObligations, maxDTI, balanceAfterExpenses);
+  const preliminaryMaxLoanSum = calculatePreliminaryMaxLoanSum(monthlyInterestRate, numberOfPayments, maxMonthlyPayment);
+  
+  
+  showMaxLoanSumResult(preliminaryMaxLoanSum);
+
+}
+
+function calculateMaxMonthlyPayment(householdIncome, householdObligations, maxDTI, balanceAfterExpenses) {  
+  // Calculate the maximum payment based on DTI
+  const maxPaymentDTI = (householdIncome - householdObligations) * (maxDTI / 100);
+    
+  return Math.min(maxPaymentDTI, balanceAfterExpenses);
+}
+
+function calculatePreliminaryMaxLoanSum(monthlyInterestRate, numberOfPayments, maxMonthlyPayment) {
+    if (monthlyInterestRate === 0) {
+        return maxMonthlyPayment * numberOfPayments; // No need to negate
+    } else {
+        return (maxMonthlyPayment * (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments)) / monthlyInterestRate);
+    }
+}
+
+function showMaxLoanSumResult(preliminaryMaxLoanSum) { 
+  document.getElementById("preliminary_max_loan").innerHTML = preliminaryMaxLoanSum.toFixed(2) + " €";
+  document.getElementById("result_pop_up").style.display = "flex";
+  document.getElementById("opacity_container").style.display = "block";
+}
